456641241b7ff667bb6b2daf757076827d5498b4
Arun TR <aruntr369@gmail.com>
2022-04-08 01:02:11 +0530

    give number getcomment()

--- app/src/main/java/com/arun/retrofitsample/JSONPlaceholder.java
+++ app/src/main/java/com/arun/retrofitsample/JSONPlaceholder.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface JSONPlaceholder {
 
     @GET("posts")
     Call<List<Post>> getPost();
 
-    @GET("posts/1/comments")
-    Call<List<Comment>> getComments();
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
 }
